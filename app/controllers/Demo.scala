6221430328d1841a021b57916f161de26818e44a
Petr Bělohlávek <me@petrbel.cz>
2014-09-03 10:59:14 +0200

    deployed on St zář  3 10:59:09 CEST 2014

--- app/controllers/Demo.scala
+++ app/controllers/Demo.scala
@@ -1,11 +1,18 @@
 package controllers
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import play.api.libs.json.Json
 import play.api.mvc.Action
 import play.api.mvc.Controller
 
 object Demo extends Controller {
-	def translate(code: String) = Action { implicit request =>
-		Ok(views.html.index())
+	def translate = Action(parse.json) { implicit request =>
+		// Async {
+			val source = request.body.\("source")
+			val result = s"Result from server! Original request: $source"	// TODO
+			Ok(result)
+			// Ok(Json.toJson(result))
+		// }
 	}
 }
