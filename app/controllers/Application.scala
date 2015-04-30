97501aac13204d50ebbcf9109f617a43c8f5144a
Petr Bělohlávek <me@petrbel.cz>
2015-04-30 11:33:00 +0200

    absolute

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import java.io.File
+import java.net._
 
 import play.Play
 import play.api.mvc.Action
@@ -12,24 +13,22 @@ object Application extends Controller {
 	def index(any: String) = Action { Ok(views.html.index()) }
 
 	/** resolve "any" into the corresponding HTML page URI */
-	def getURI(any: String): String = s"/public/html/$any.html"
+	def getURI(any: String, host: String=""): String = s"$host/public/html/$any.html"
 
 	/** load an HTML page from public/html */
 	def loadPublicHTML(any: String) = Action { implicit request =>
-		val projectRoot = Play.application.path()
-		var file = new File(projectRoot + getURI(any))
-		println(file.getAbsolutePath())
-		println(file.exists())
-		
-		//println(request.getBase())
-		//println(request.url)
-		println(request.domain)
-		println(request.host)
+		//val projectRoot = Play.application.path()
+		//var file = new File(projectRoot + getURI(any))
+		//var file = new File(getURI(any, request.host))
 
 
-		if (file.exists())
-			Ok(scala.io.Source.fromFile(file.getCanonicalPath(), "UTF-8").mkString).as("text/html");
-		else
-			NotFound
+		val url = new URL("http://" + getURI(any, request.host))
+		println(url)
+
+		//if (file.exists())
+			//Ok(scala.io.Source.fromFile(file.getCanonicalPath(), "UTF-8").mkString).as("text/html");
+			Ok(scala.io.Source.fromURL(url).mkString).as("text/html");
+		//else
+		//	NotFound
 	}
 }
