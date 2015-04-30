a43cec7c2d091e625257790e571b71989d3a4d98
Petr Bělohlávek <me@petrbel.cz>
2015-04-30 14:10:31 +0200

    cleanup

--- app/controllers/Demo.scala
+++ app/controllers/Demo.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.io._
+
 import scala.collection.immutable.::
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -7,14 +9,16 @@ import play.api.libs.json.Json
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import play.api.libs.Files.TemporaryFile
-import java.io._
+
 import org.omp4j.Config
 import org.omp4j.preprocessor.Preprocessor
+import org.omp4j.utils.FileTreeWalker
 
 object Demo extends Controller {
 	def translate = Action(parse.json) { implicit request =>
 		val f_in = File.createTempFile("pre", ".java")
-		//val f_out = File.createTempFile("post", ".java")
+		var f_out: File = null
+		var tmpDir2delete: File = null
 
 		val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f_in)))
 
@@ -24,18 +28,14 @@ object Demo extends Controller {
 
 			val code: String = source.toString
 			writer.write(StringContext treatEscapes code.substring(1, code.length-1))
-			/*
-			code.substring(1, code.length-1).split("""\\n""").foreach{ chunk =>
-				writer write (StringContext treatEscapes chunk)
-				writer.newLine()
-			}*/
 			writer.flush()
 
 			val conf = new Config(Array("-d", System.getProperty("java.io.tmpdir"), "--source-only", f_in.getAbsolutePath))     // set up configuration based on program arguments
 			val prep = new Preprocessor()(conf)     // create preprocessor
 
 			val (outfiles, (tmpDir, prepDir)::_) = prep.run()        // fetch the array of (already saved) preprocessed files
-			val f_out: File = outfiles.head
+			f_out = outfiles.head
+			tmpDir2delete = tmpDir
 
 			val output = scala.io.Source.fromFile(f_out).mkString
 			Ok(output)
@@ -44,6 +44,10 @@ object Demo extends Controller {
 			case e: Exception => Ok(e.getMessage)
 		} finally {
 			writer.close()
+			f_in.delete()
+			f_out.delete()
+			FileTreeWalker.recursiveDelete(tmpDir2delete)
+
 		}
 	}
 }
